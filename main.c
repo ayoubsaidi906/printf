6ac094a15e9a074684485d746c3f4df52dc6d432
hyperayoub <bouagnaayoub@gmail.com>
2023-10-20 03:21:07 -0700

    update

---
+++ main.c
@@ -0,0 +1,57 @@
+#include <limits.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+    int len;
+    int len2;
+    unsigned int ui;
+    void *addr;
+    len = _printf("Let's try to printf a simple sentence.\n");
+    len2 = printf("Let's try to printf a simple sentence.\n");
+    ui = (unsigned int)INT_MAX + 1024;
+    addr = (void *)0x7ffe637541f0;
+    _printf("Length:[%d, %i]\n", len, len);
+    printf("Length:[%d, %i]\n", len2, len2);
+    _printf("Negative:[%d]\n", -762534);
+    printf("Negative:[%d]\n", -762534);
+    _printf("Unsigned:[%u]\n", ui);
+    printf("Unsigned:[%u]\n", ui);
+    _printf("Unsigned octal:[%o]\n", ui);
+    printf("Unsigned octal:[%o]\n", ui);
+    _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+_printf("Character:[%c]\n", 'H');
+printf("Character:[%c]\n", 'H');
+_printf("String:[%s]\n", "I am a string !");
+printf("String:[%s]\n", "I am a string !");
+_printf("Address:[%p]\n", addr);
+printf("Address:[%p]\n", addr);
+len = _printf("Percent:[%%]\n");
+len2 = printf("Percent:[%%]\n");
+_printf("Len:[%d]\n", len);
+printf("Len:[%d]\n", len2);
+_printf("Unknown:[%r]\n");
+n = _printf("DZEKO YOHIBO STRANGER\n");
+printf("%d\n", n);
+len = _printf("[%s]\n", "hi hi hi");
+len2 =printf("[%s]\n", "hi hi hi");
+len =printf("Len:[%d]\n", len);
+len2 =printf("Len:[%d]\n", len2);
+len = _printf("wach khdmat: %c. gul lya?\n", 48);
+_printf("%-1\n");
+_printf("%i\n",567);
+_printf("%d\n",345);
+ _printf("%b\n", 98);
+ _printf("%b\n", 2653623);
+ _printf("%b\n", 1);
+ _printf("%b\n", 0);
+return (0);
+}
