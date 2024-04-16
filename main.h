b2f3d1265ef3ae7e2756261812adb02fd1c8064a
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-04-16 23:56:15 +0100

    Update main.h

--- main.h
+++ main.h
@@ -8,7 +8,6 @@
 int _putchar(char a);
 int pu_s(const char *b);
 void handle_for(va_list arg, char specifier, int *i);
-int  p_b(unsigned int a);
 int _printf(const char *format, ...);
 int p_n(int ayoub);
 int print_mybin(va_list val)
