f162bcd221f806de6b05ae68cf838f00ab7d86ac
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-04-16 23:13:36 +0100

    Update a_handle_for.c

--- a_handle_for.c
+++ a_handle_for.c
@@ -1,37 +1,21 @@
-#include"main.h"
+#include "main.h"
+
 /**
- * handle_for - handles different specifiers
- * @i: the counter
+ * handle_for - handles different specifiers 'd' and 'i'
+ * @arg: argument list
  * @specifier: the specifier
- * @arg: argument
+ * @i: pointer to the counter
  */
 void handle_for(va_list arg, char specifier, int *i)
 {
-	switch (specifier)
-	{
-		case 'b':
-				(*i) += p_b(va_arg(arg, unsigned int));
-				break;
-		case 'c':
-				(*i) += _putchar(va_arg(arg, int));
-				break;
-		case 'i':
-		                (*i) += _putchar(var_arg(arg, int));
-		                break;
-		case 'd':
-				(*i) += p_n(va_arg(arg, int));
-				break;
-		case 's':
-				(*i) += pu_s(va_arg(arg, char *));
-				break;
-		case '%':
-				(*i) += _putchar('%');
-				break;
-
-		default:
-				_putchar('%');
-				_putchar(specifier);
-				(*i) += 2;
-				break;
-	}
+    if (specifier == 'd' || specifier == 'i')
+    {
+        (*i) += p_n(va_arg(arg, int));
+    }
+    else
+    {
+        _putchar('%');
+        _putchar(specifier);
+        (*i) += 2;
+    }
 }
