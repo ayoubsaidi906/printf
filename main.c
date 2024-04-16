55a47745e85a2835d65e08d34c56de784ac6bae2
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-04-16 23:36:36 +0100

    Update main.c

--- main.c
+++ main.c
@@ -9,24 +9,24 @@
  */
 int main(void)
 {
-	int n;
-    int len;
-    int len2;
-    unsigned int ui;
-    void *addr;
-    len = _printf("Let's try to printf a simple sentence.\n");
-    len2 = printf("Let's try to printf a simple sentence.\n");
-    ui = (unsigned int)INT_MAX + 1024;
-    addr = (void *)0x7ffe637541f0;
-    _printf("Length:[%d, %i]\n", len, len);
-    printf("Length:[%d, %i]\n", len2, len2);
-    _printf("Negative:[%d]\n", -762534);
-    printf("Negative:[%d]\n", -762534);
-    _printf("Unsigned:[%u]\n", ui);
-    printf("Unsigned:[%u]\n", ui);
-    _printf("Unsigned octal:[%o]\n", ui);
-    printf("Unsigned octal:[%o]\n", ui);
-    _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+int n;
+int len;
+int len2;
+unsigned int ui;
+void *addr;
+len = _printf("Let's try to printf a simple sentence.\n");
+len2 = printf("Let's try to printf a simple sentence.\n");
+ui = (unsigned int)INT_MAX + 1024;
+addr = (void *)0x7ffe637541f0;
+_printf("Length:[%d, %i]\n", len, len);
+printf("Length:[%d, %i]\n", len2, len2);
+_printf("Negative:[%d]\n", -762534);
+printf("Negative:[%d]\n", -762534);
+_printf("Unsigned:[%u]\n", ui);
+printf("Unsigned:[%u]\n", ui);
+_printf("Unsigned octal:[%o]\n", ui);
+printf("Unsigned octal:[%o]\n", ui);
+_printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
 printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
 _printf("Character:[%c]\n", 'H');
 printf("Character:[%c]\n", 'H');
@@ -49,9 +49,9 @@ len = _printf("wach khdmat: %c. gul lya?\n", 48);
 _printf("%-1\n");
 _printf("%i\n",567);
 _printf("%d\n",345);
- _printf("%b\n", 98);
- _printf("%b\n", 2653623);
- _printf("%b\n", 1);
- _printf("%b\n", 0);
+_printf("%b\n", 98);
+_printf("%b\n", 2653623);
+_printf("%b\n", 1);
+_printf("%b\n", 0);
 return (0);
 }
